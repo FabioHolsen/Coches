f6c530d541bc7b90562b67d5668f9dda1060be6a
Fabio <fabioholsen@gmail.com>
2025-05-10 05:05:36 +0200

    i

--- aplicacion/ConsultaCochesGama.php
+++ aplicacion/ConsultaCochesGama.php
@@ -55,7 +55,7 @@
                             if ($result->num_rows > 0) {
                             while($row = $result->fetch_assoc()) {
                             echo "
-                            <h1>Gama {$row['nomgama']}</h1>
+                            <h1>Gama {$row['nomgama']}</h1>";
                             
                             }
                             }
