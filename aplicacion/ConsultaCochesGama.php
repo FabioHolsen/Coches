574e8b3217fff9f097812c539280eee6a6aa5e79
Fabio <fabioholsen@gmail.com>
2025-05-10 05:06:44 +0200

    u

--- aplicacion/ConsultaCochesGama.php
+++ aplicacion/ConsultaCochesGama.php
@@ -55,7 +55,7 @@
                             if ($result->num_rows > 0) {
                             while($row = $result->fetch_assoc()) {
                             echo "
-                            <table class="center tabla">
+                            <table class='center tabla'>
                                 <thead>
                                     <tr>
                                         <th>Matricula</th>
