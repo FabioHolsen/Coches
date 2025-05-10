b89bbd319a2fa7005d09ce4b7b7bf8bb70e64b75
Fabio <fabioholsen@gmail.com>
2025-05-10 04:56:42 +0200

    E

--- aplicacion/ConsultaCochesGama.php
+++ aplicacion/ConsultaCochesGama.php
@@ -48,29 +48,29 @@
                 <?php
                             // Consultar los datos
                             $sql = "SELECT * FROM coche inner join gama on coche.codgama = gama.codgama  where coche.codgama = 'F1'";
-                            $result1 = $conn->query($sql);
+                            $result = $conn->query($sql);
                             if ($result === false) {
                             die("Error en la consulta: " . $conn->error);
                             }
                             if ($result1->num_rows > 0) {
-                            while($row1 = $result1->fetch_assoc()) {
+                            while($row = $result1->fetch_assoc()) {
                             echo "
-                            <h1>Gama {$row1['nomgama']}</h1>
+                            <h1>Gama {$row['nomgama']}</h1>
                             <table class="center tabla">
-                            <thead>
-                                <tr>
-                                    <th>Matricula</th>
-                                    <th>Modelo</th>
-                                    <th>Consulta del coche</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>{$row1['matricula']}</td>
-                                    <td>{$row1['modelo']}</td>
-                                    <td><a href='consulta_coche.php?matricula={$row1['matricula']}'>Ver más</a></td>
-                                </tr>
-                            </tbody>
+                                <thead>
+                                    <tr>
+                                        <th>Matricula</th>
+                                        <th>Modelo</th>
+                                        <th>Consulta del coche</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>{$row['matricula']}</td>
+                                        <td>{$row['modelo']}</td>
+                                        <td><a href='consulta_coche.php?matricula={$row['matricula']}'>Ver más</a></td>
+                                    </tr>
+                                </tbody>
                             </table>";
                             }
                             }
