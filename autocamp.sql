62ac656dec88be877e26417320eac1e7604acbd1
Fabio <fabioholsen@gmail.com>
2025-05-13 02:06:52 +0200

    cas

--- autocamp.sql
+++ autocamp.sql
@@ -36,7 +36,7 @@ CREATE TABLE reserva (
     fecha_res date NOT NULL,
     f_inicio date,
     f_fin date,
-    dias cast(datediff(day,f_inicio,f_fin)),
+    dias as cast(datediff(day,f_inicio,f_fin) as integer),
     lugar character varying(50),
     codgama character varying(2) NOT NULL,
     codcliente int NOT NULL,
